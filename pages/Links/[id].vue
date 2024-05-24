2a1049914e8fbffa2ac6f5acdda6f6e1e0bb633e
Armel K. Dovenon <dove.armel@gmail.com>
2024-05-24 17:22:45 +0100

    création de differentes pages pour les liens

--- pages/Links/[id].vue
+++ pages/Links/[id].vue
@@ -1,6 +1,22 @@
 <template>
-  <div>
-    page [id]
+  <div class=" prose mx-auto max-w-md">
+
+   
+    <h1>Modifier le lien</h1>
+    <GoBack />
+
+    <form action="">
+      <label for="">
+        <div>Lien Court</div>
+        <input type="text">
+      </label>
+
+      <label for="">
+        <div>Lien Complet</div>
+        <input type="text">
+      </label>
+      <button>Modifier le lien</button>
+    </form>
   </div>
 </template>
 
@@ -8,6 +24,12 @@
 
 </script>
 
-<style>
+<style scoped>
+  label {
+    @apply block mb-2 text-sm font-medium text-gray-900;
+  }
 
+  input {
+    @apply bg-gray-50 border border-gray-300 text-gray-900 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5;
+  }
 </style>
--- pages/Links/index.vue
+++ pages/Links/index.vue
@@ -1,10 +1,78 @@
 <template>
     <h2>Mes Liens</h2>
+
+    <table class="w-full boder text-sm text-sm text-left text-gray-500">
+        <thead class="text-xs text-gay-700 uppercase bg-gray-50">
+            <tr>
+                <th >Lien complet</th>
+                <th >Lien Court</th>
+                <th >Nbr. Vues</th>
+                <th >Supprimer</th>
+                <th >Editer</th>
+                <th  aria-label="Rafraichir">
+                    <Icon name="refresh" />
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="link in links" :key="link.id" class="bg-white border-b">
+                <td> 
+                    <a :href="link.full_links" target="_blank">
+                        {{ link.full_links }}
+                    </a>
+                </td>
+                <td> 
+                    <a :href="`${useRuntimeConfig().public.appUrl}/${link.short_links}`" target="_blank">
+                        {{ `${useRuntimeConfig().public.appUrl.replace(/^http(s?):\/\//, "")}/${link.short_links}` }}
+                    </a>
+                </td>
+                <td> 
+                    {{ link.views }}
+                </td>
+                <td> 
+                    <Icon name="mdi:trash" />
+                </td>
+                <td> 
+                    <NuxtLink :to="`/links/${link.id}`">
+                        <Icon name="material-symbols:edit-square-outline" />
+                    </NuxtLink>
+                </td>
+                <td> </td>
+            </tr>
+        </tbody>
+
+    </table>
 </template>
 <script lang="ts" setup>
+    const links = [
+        {
+            short_links: "lk34oigfd",
+            full_links: "https://youtube.com",
+            views: 3,
+            id: 1,
 
+        },
+        {
+            short_links: "rtg244fgh",
+            full_links: "https://w3school.com",
+            views: 1,
+            id: 2,
+        },
+        {
+            short_links: "5d4fghhder",
+            full_links: "https://devdocs.io",
+            views: 0,
+            id: 3,
+        }
+    ]
 </script>
 
-<style>
-
+<style scoped>
+th {
+    @apply px-6 py-3
+}
+td {
+    @apply px-6 py-4
+}
+    
 </style>

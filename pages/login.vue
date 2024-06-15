e4ed36a2a164e01f7205a42674c60ac3a893d683
Armel K. Dovenon <dove.armel@gmail.com>
2024-06-15 11:26:40 +0100

    création des vue login et register

---
+++ pages/login.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="prose">
+    <h1>Connexion</h1>
+    <form>
+      <label >
+        Email
+        <input type="email">
+      </label>
+      <label >
+        Mot de passe
+        <input type="password">
+      </label>
+      <button  class="btn">Se Connecter</button>
+    </form>
+    <p>Pas de compte ?
+      <NuxtLink to="/register" class = "underline text-blue-400">Inscrivez-vous !</NuxtLink>
+    </p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  definePageMeta({
+    layout: 'centered'
+  })
+</script>
+
+<style>
+
+</style>
---
+++ pages/register.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="prose">
+    <h1>Inscription</h1>
+    <form>
+      <label >
+        Nom et prénom
+        <input type="text">
+      </label>
+      <label >
+        Email
+        <input type="email">
+      </label>
+      <label >
+        Mot de passe
+        <input type="password">
+      </label>
+      <label >
+        Confirmation Mot de passe
+        <input type="password">
+      </label>
+      <button  class="btn">S'inscrire</button>
+    </form>
+    <p>Vous avez déjà un compte ?
+      <NuxtLink to="/login" class = "underline text-blue-400">Inscrivez-vous !</NuxtLink>
+    </p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  definePageMeta({
+    layout: 'centered'
+  })
+</script>
+
+<style>
+
+</style>
